d1059b7691d36448746ebf4c00b39431083748c7
yuvrajrathod8390 <yuvrajrathod8390@gmail.com>
2025-02-07 14:14:53 +0530

    Reverse Array

---
+++ Array/ReverseArr.java
@@ -0,0 +1,26 @@
+
+
+class ReverseArr{
+
+    static void reverse(int a[], int n)
+    {
+        int[] b = new int[n];
+        int j = n;
+        for (int i = 0; i < n; i++) {
+            b[j - 1] = a[i];
+            j = j - 1;
+        }
+
+        // printing the reversed array
+        System.out.println("");
+        for (int k = 0; k < n; k++) {
+            System.out.println(b[k]);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int [] arr = {1, 2, 3, 4, 5};
+        reverse(arr, arr.length);
+    }
+}
