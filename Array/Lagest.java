2c4894f9c4bd2ca4e1f65e64f3ab4e00865e2380
yuvrajrathod8390 <yuvrajrathod8390@gmail.com>
2025-02-07 16:32:29 +0530

    largest element

--- Array/Lagest.java
+++ Array/Lagest.java
@@ -1,24 +1,32 @@
 import java.util.Arrays;
 
 class Lagest {
-public static int getSecondlargest(int[] arr){
-    int n=arr.length;
-    Arrays.sort(arr);
-    int max=-1 ;
-    for(int i=0; i<n;i++){
-        if(arr[i]<max){
-            max=arr[i];
+    static int getSecondLargest(int[] arr) {
+        int n = arr.length;
+
+        int largest = -1, secondLargest = -1;
+
+        // Finding the largest element
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
         }
+
+        // Finding the second largest element
+        for (int i = 0; i < n; i++) {
+            
+            // Update second largest if the current element is greater
+            // than second largest and not equal to the largest
+            if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
     }
-
-
-
-return max;
-}
     public static void main(String[] args) {
         System.out.println("Find the second largest element in an array\n\n");
         int[] arr={56,89,46,25,87,46};
-        System.out.println(getSecondlargest(arr));
+        System.out.println(getSecondLargest(arr));
 
 
     }
